2724900ec9f4f4eee684cfe7a14a0ad1d6663565
MrinskiyAndrey <nomarg@yandex.ru>
2022-01-11 15:34:17 +0300

    no localhost

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.product (name, brand, price, weight) VALUES ('Протеин', 'Optimum Nutrition', 5579, 2270);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('BCAA', 'Gold Nutrition', 1912, 454);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('BCAA', 'Xtend', 1991, 420);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('Креатин', 'ALLMAX Nutrition', 1967, 400);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('Изотоник', 'Ultima Replenisher', 3346, 306);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('Коллаген', 'Gold Nutrition', 2789, 464);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('Глютамин', 'Gold Nutrition', 1275, 454);
+INSERT INTO public.product (name, brand, price, weight) VALUES ('Казеин', 'California Gold Nutrition', 1115, 454);
